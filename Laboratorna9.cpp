1e34a14e952da38d4cd42eb04a71e937dcffd5bf
Алла <@DESKTOP-QMLP632>
2024-10-30 22:26:27 +0200

    Add project files.

---
+++ Laboratorna9.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include<queue>
+#include "Windows.h"
+
+using namespace std;
+
+int main()
+{
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    queue<int> q;
+    int n, x, sum = 0;
+
+    cout << "Введіть розмір черги: ";
+    cin >> n; 
+
+    cout << "Введіть "<<n<<" цілих чисел:\n ";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        q.push(x);
+    }
+    while (!q.empty()) {
+        if (q.front() % 2 == 0) {
+            sum += q.front();
+        }
+        q.pop();
+    }
+    cout << "Сума парних елементів у черзі:"<< sum <<endl;
+    return 0;
+
+
+}
+
